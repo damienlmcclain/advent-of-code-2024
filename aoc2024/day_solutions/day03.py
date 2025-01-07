2260a40d7ed8b705f4e006147c83592a9a06f01b
Damien McClain <122814213+damienlmcclain@users.noreply.github.com>
2025-01-07 16:39:34 -0500

    Add day 3 part 2

--- aoc2024/day_solutions/day03.py
+++ aoc2024/day_solutions/day03.py
@@ -39,7 +39,8 @@ def check_substring(substring: str) -> List:
     return num_list
 
 
-def parse_input(corrupted_input: str) -> List[List[int]]:
+def parse_input(corrupted_input: str,
+                conditional: bool = False) -> List[List[int]]:
     """
     Parses the input string to find acceptable int pairs.
 
@@ -47,6 +48,8 @@ def parse_input(corrupted_input: str) -> List[List[int]]:
     ----------
     corrupted_input
         The string to parse.
+    conditional
+        True if conditionals need to be considered. False if they don't.
 
     Returns
     -------
@@ -58,15 +61,55 @@ def parse_input(corrupted_input: str) -> List[List[int]]:
         i.start() for i in re.finditer(str_to_find, corrupted_input)
     ]
 
-    for idx in mul_indexes:
-        check = check_substring(corrupted_input[idx + len(str_to_find):])
-        if check:
-            list_of_nums.append(check)
+    if conditional is True:
+        # start with 0 because mul instructions are enabled at the beginning
+        do_indexes = [0]
+        do_indexes.extend(
+            [i.start() for i in re.finditer('do\(\)', corrupted_input)])
+        dont_indexes = [
+            i.start() for i in re.finditer("don\'t\(\)", corrupted_input)
+        ]
+
+        while mul_indexes:
+            if not do_indexes:
+                return list_of_nums
+            elif not dont_indexes:
+                # no more don'ts so evaluate everything remaining
+                muls_to_evaluate = []
+                for idx in mul_indexes:
+                    if idx > do_indexes[0]:
+                        muls_to_evaluate.append(idx)
+                for value in muls_to_evaluate:
+                    check = check_substring(corrupted_input[value +
+                                                            len(str_to_find):])
+                    if check:
+                        list_of_nums.append(check)
+                    mul_indexes = mul_indexes[1:]
+                return list_of_nums
+            elif do_indexes[0] > dont_indexes[0]:
+                dont_indexes = dont_indexes[1:]
+            elif do_indexes[0] < mul_indexes[0] < dont_indexes[0]:
+                check = check_substring(
+                    corrupted_input[mul_indexes[0] +
+                                    len(str_to_find):dont_indexes[0]])
+                if check:
+                    list_of_nums.append(check)
+                mul_indexes = mul_indexes[1:]
+            elif do_indexes[0] < mul_indexes[0] and dont_indexes[
+                    0] < mul_indexes[0]:
+                do_indexes = do_indexes[1:]
+            else:
+                mul_indexes = mul_indexes[1:]
+    else:
+        for idx in mul_indexes:
+            check = check_substring(corrupted_input[idx + len(str_to_find):])
+            if check:
+                list_of_nums.append(check)
 
     return list_of_nums
 
 
-def sum_num_lists(corrupted_input: str) -> int:
+def sum_num_lists(corrupted_input: str, conditional: bool = False) -> int:
     """
     Parses the input list for acceptable ints. Then multiplies each sublist
     before summing them.
@@ -75,13 +118,16 @@ def sum_num_lists(corrupted_input: str) -> int:
     ----------
     corrupted_input
         The input to parse.
+    conditional
+        True if conditionals need to be considered. False if they don't.
 
     Returns
     -------
     The total sum of the product of each int pair.
     """
 
-    num_list = parse_input(corrupted_input=corrupted_input)
+    num_list = parse_input(corrupted_input=corrupted_input,
+                           conditional=conditional)
 
     total_sum = 0
 
--- aoc2024/day_solutions/tests/test_day03.py
+++ aoc2024/day_solutions/tests/test_day03.py
@@ -7,6 +7,8 @@ from aoc2024.session_cookie import AOCD_SESSION_COOKIE
 
 
 class Test(TestCase):
+    os.environ['AOC_SESSION'] = AOCD_SESSION_COOKIE
+    corrupted_input = aocd.get_data(day=3, year=2024)
 
     def test_parse_input(self):
         corrupted_input = 'xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))'
@@ -20,7 +22,15 @@ class Test(TestCase):
         self.assertEqual(res, 161)
 
     def test_sum_num_lists_full_data(self):
-        os.environ['AOC_SESSION'] = AOCD_SESSION_COOKIE
-        corrupted_input = aocd.get_data(day=3, year=2024)
-        res = sum_num_lists(corrupted_input=corrupted_input)
+        res = sum_num_lists(corrupted_input=self.corrupted_input)
         self.assertEqual(res, 175015740)
+
+    def test_sum_num_lists_conditional(self):
+        corrupted_input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))don't()mul(2,3)"
+        res = sum_num_lists(corrupted_input=corrupted_input, conditional=True)
+        self.assertEqual(res, 48)
+
+    def test_sum_num_lists_conditional_full_data(self):
+        res = sum_num_lists(corrupted_input=self.corrupted_input,
+                            conditional=True)
+        self.assertEqual(res, 112272912)
